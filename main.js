4204de978826a7e3f27a7050f45b2e297b27ef7a
Kalys01 <kalysosmon96.g@gmail.com>
2021-02-12 19:01:39 +0600

    added styles

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 let msg = document.querySelector('#msg-text');
 let btnspeak = document.querySelector('#speak');
 
+
 function speak(text) {
     const message = new SpeechSynthesisUtterance();
-    message.lang = 'en-US';
+    message.lang = 'ru-RU';
     message.text = text;
     window.speechSynthesis.speak(message);
 }
