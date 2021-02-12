fb16a358dfafd9ebbf96797d5e60fa567cd0d0a8
Kalys01 <kalysosmon96.g@gmail.com>
2021-02-12 20:39:36 +0600

    added a blank to the check

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 let msg = document.querySelector('#msg-text');
 let btnspeak = document.querySelector('#speak');
+let btnclear = document.querySelector('#clear');
 
+function check(){
+    if (msg.value) {
+        btnclear.classList.remove('empty');
+        if (empty(msg.value)) {
+            btnspeak.classList.remove('empty');
+        } 
+    }else {
+        btnspeak.classList.add('empty');
+        btnclear.classList.add('empty');
+    }
+}
+
+function empty(s){
+    if(s.trim() !== '')
+        return true;
+    return false;
+}
 
 function speak(text) {
     const message = new SpeechSynthesisUtterance();
@@ -8,6 +26,15 @@ function speak(text) {
     message.text = text;
     window.speechSynthesis.speak(message);
 }
+
+function fclear() {
+    msg.value = "";
+    btnspeak.classList.add('empty');
+    btnclear.classList.add('empty');
+}
+
 btnspeak.addEventListener('click', function() {  
     speak(msg.value);
-  });
+});
+btnclear.addEventListener('click', fclear);
+msg.addEventListener('input', check);
